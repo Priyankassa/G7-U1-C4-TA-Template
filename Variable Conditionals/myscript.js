d08ce99b25a4ddbd2985e8ffd52aa329b5abcd18
unknown <raveen.anand@whitehatjr.com>
2021-10-14 17:44:58 +0530

    v2

--- Variable Conditionals/myscript.js
+++ Variable Conditionals/myscript.js
@@ -1,2 +1,21 @@
+function check()
+{
+    /**Take input value from html element 
+     with id "word" and store it in a variable named input**/
+    var input = document.getElementById("")
+    
+    /**Print "input" variable to console**/
+    console.log();
 
+    /**Calculate length of input 
+    and store it in a variable named count**/
+    
+
+    /**Print "count" variable to console**/
+    
+    
+    /**Add if-else to check if count is greater than 5 or lesser 
+    and print a message to the console accordingly***/
+    
+}
 
